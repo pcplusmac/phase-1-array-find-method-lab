b5751fa32693e1fd8f4854302ca6f800551231ef
el-sam <samurai.h@yahoo.com>
2023-10-21 13:53:16 +0800

    pass the test fo rreturn found result

--- index.js
+++ index.js
@@ -1,1 +1,22 @@
-// code your solution here
+// Define an array
+const record = [
+    { year: "2015", result: "W"},
+    { year: "2014", result: "N/A"},
+    { year: "2013", result: "L"},
+    //...
+  ]
+
+ 
+//   Define a method  'superbowlWin'
+function  superbowlWin(arr) {
+    let item = arr.find(element => element.result === "W");
+    
+    return item["year"];
+}       
+    
+
+
+// find the element based on the conditon 
+superbowlWin(record);
+
+
